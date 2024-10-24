f854fef4aed77a44cb97f1ba6a1dba1ec2f7451d
Robyn Lo <lo.yiwen@proton.me>
2024-10-24 01:51:51 +0100

    docs: added docstrings and corrected typo

--- config.py
+++ config.py
@@ -3,17 +3,21 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
+    """Base Configuration Class"""
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 class DevelopmentConfig(Config):
+    """Development Configuration"""
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'dev.db')
     DEBUG = True
 
 class TestingConfig(Config):
+    """Testing Configuration"""
     SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
     TESTING = True
     DEBUG = True
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'abb.db')
+    """Production Configuration"""
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
     DEBUG = False

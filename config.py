a8657436b60b5fe055ad297c1e1d4d88d41ef970
Robyn Lo <lo.yiwen@proton.me>
2024-10-24 03:37:17 +0100

    chore: disabled CSRF protection for testing configuration

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     """Base configuration class"""
     SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'a_secret_key'
 
 class DevelopmentConfig(Config):
     """Development configuration"""
@@ -16,6 +17,7 @@ class TestingConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
     TESTING = True
     DEBUG = True
+    WTF_CSRF_ENABLED = False
 
 class ProductionConfig(Config):
     """Production configuration"""

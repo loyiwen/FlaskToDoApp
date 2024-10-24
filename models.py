23c2cfbfc50bc05a1479e0af043527d12b665c9d
Robyn Lo <lo.yiwen@proton.me>
2024-10-24 01:49:53 +0100

    feat: created custom getter and setter for `deadline` field
    - If the input value is a string, it is converted to a Python datetime object before being set into the database
    - If the input value is already a datetime object it is set directly

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
 
@@ -6,6 +7,21 @@ class Assessment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     module_code = db.Column(db.String(10), nullable=False)
-    deadline = db.Column(db.DateTime, nullable=False)
+    _deadline = db.Column('deadline', db.DateTime, nullable=False)
     description = db.Column(db.Text, nullable=False)
     is_complete = db.Column(db.Boolean, default=False)
+
+    @property
+    def deadline(self):
+        """Getter for deadline."""
+        return self._deadline
+    
+    @deadline.setter
+    def deadline(self, value):
+        """Setter for deadline that accepts both string and datetime."""
+        if isinstance(value, str):
+            self._deadline = datetime.strptime(value, '%Y-%m-%d')
+        elif isinstance(value, datetime):
+            self._deadline = value
+        else:
+            raise ValueError("Invalid format for deadline. Must be a string.")

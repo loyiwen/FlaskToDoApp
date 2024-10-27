b25a7f9cb32db0bfc1dded8c0fff40aeb1efe487
Robyn Lo <lo.yiwen@proton.me>
2024-10-27 01:30:38 +0100

    feat: displays all assessments on home page and redirects on assessment creation
    - Displays all assessments stored in the dev/db database on the home page route
    - Redirects /create route to home page after an assessment is successfully created
    - Cleaned up if/else logic in /create route

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request
-from config import DevelopmentConfig
-from forms import AssessmentForm
+from flask import Flask, redirect, render_template, url_for, request
 from models import db, Assessment
+from forms import AssessmentForm
+from config import DevelopmentConfig
 
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
@@ -9,9 +9,12 @@ app.config.from_object(DevelopmentConfig)
 # Initialise SQLAlchemy with app
 db.init_app(app)
 
+
 @app.route('/')
 def index():
-    return "Home Page Text"
+    assessments = Assessment.query.all()
+    return render_template('index.html', assessments=assessments)
+
 
 # Create new assessment (handle GET and POST requests)
 @app.route('/create', methods=['GET', 'POST'])
@@ -24,19 +27,22 @@ def create():
             print("Form validation failed. Errors:", form.errors)
             return render_template('create.html', form=form)
         
-        new_assessment = Assessment(
-            title=form.title.data,
-            module_code=form.module_code.data,
-            deadline=form.deadline.data,
-            description=form.description.data,
-            is_complete=form.is_complete.data
-        )
-        db.session.add(new_assessment)
-        db.session.commit() 
-        return "Assessment Created", 200  # Simple success message for test verification
-
-    # If GET request, just render the empty form
-    return render_template('create.html', form=form)
+        else:
+            new_assessment = Assessment(
+                title=form.title.data,
+                module_code=form.module_code.data,
+                deadline=form.deadline.data,
+                description=form.description.data,
+                is_complete=form.is_complete.data
+            )
+            db.session.add(new_assessment)
+            db.session.commit() 
+            return redirect(url_for('index'))
+    else:
+        # If GET request, just render the empty form
+        return render_template('create.html', form=form)
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)

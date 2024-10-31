dd201455f5129248a0e2b122ccf9e97948d4c908
Robyn Lo <lo.yiwen@proton.me>
2024-10-31 04:25:28 +0000

    feat: added fade-out functionality to flash messages and resized width of message bubble to match text

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,5 +1,4 @@
 // scripts.js
-
 function toggleCheckbox(iconElement, assessmentId) {
     // Toggle the icon between checked and unchecked
     if (iconElement.textContent === 'check_box') {
@@ -11,3 +10,14 @@ function toggleCheckbox(iconElement, assessmentId) {
     // Submit the form to toggle the completion state in the backend
     document.getElementById(`completion-form-${assessmentId}`).submit();
 }
+
+setTimeout(function() {
+    const flashMessages = document.querySelectorAll('.alert');
+    flashMessages.forEach(message => {
+        message.style.transition = 'opacity 1s';
+        message.style.opacity = '0';
+        
+        // Remove the message from the DOM after fading out
+        setTimeout(() => message.remove(), 1000);
+    });
+}, 3000);
